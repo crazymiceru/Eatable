b057c7144847cf90e9d322a003ab0aed5809d105
crazymiceru <63462709+crazymiceru@users.noreply.github.com>
2021-10-07 21:22:59 +0300

    Added eventbus sample Fixed the size of the card

---
+++ Assets/Scripts/Utils/EventBus.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class EventBus
+{
+    //int
+    private static EventBus<int> EB_int = new EventBus<int>();
+    public static void Subscribe(string variable, Action<int> action) => EB_int.Subscribe(variable, action);
+    public static void UnSubscribe(string variable, Action<int> action) => EB_int.UnSubscribe(variable, action);
+    public static void RaiseEvent(string variable, int value) => EB_int.RaiseEvent(variable, value);
+
+    //float
+    private static EventBus<float> EB_float = new EventBus<float>();
+    public static void Subscribe(string variable, Action<float> action) => EB_float.Subscribe(variable, action);
+    public static void UnSubscribe(string variable, Action<float> action) => EB_float.UnSubscribe(variable, action);
+    public static void RaiseEvent(string variable, float value) => EB_float.RaiseEvent(variable, value);
+}
+
+public class EventBus<T>
+{
+    private Dictionary<string, List<Action<T>>> _subscribers = new Dictionary<string, List<Action<T>>>();
+
+    public void Subscribe(string variable, Action<T> action)
+    {
+        if (!_subscribers.ContainsKey(variable)) _subscribers[variable] = new List<Action<T>>();
+        _subscribers[variable].Add(action);
+    }
+    public void UnSubscribe(string variable, Action<T> action)
+    {
+        if (!_subscribers.ContainsKey(variable)) return;
+        _subscribers[variable].Remove(action);
+    }
+    public void RaiseEvent(string variable, T value)
+    {
+        if (!_subscribers.ContainsKey(variable)) return;
+        foreach (var item in _subscribers[variable])
+        {
+            item?.Invoke(value);
+        }
+    }
+}
